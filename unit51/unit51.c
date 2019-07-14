1c8a88e4bc49338c6e1442016d4700fa36e63e8a
leehyodo <h551654@gmail.com>
2019-07-15 00:19:49 +0900

    처음 코딩도장 커밋

---
+++ unit51/unit51.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+
+struct PacketHeader 
+{
+	char flags;
+	int seq;
+};
+
+int main()
+{
+	struct PacketHeader header;
+
+	printf("%d\n", sizeof(header.flags));
+	printf("%d\n", sizeof(header.seq));
+	printf("%d\n", sizeof(header));
+	printf("%d\n", sizeof(struct PacketHeader));
+
+	return 0;
+}
