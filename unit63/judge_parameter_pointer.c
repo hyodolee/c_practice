10f73ddaef2b0885622b51a6540d8827ed289011
lee_hyodo <h551654@naver.com>
2019-07-21 16:52:02 +0900

    2019_07_21 study

---
+++ unit63/judge_parameter_pointer.c
@@ -0,0 +1,26 @@
+﻿//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int  getQuotientAndRemainder(int num1, int num2, int ** remainder)
+//{
+//	int rslt = (num1 / num2);
+//
+//	*remainder = (num1 % num2);
+//
+//	return rslt;
+//}
+//
+//int main()
+//{
+//	int num1;
+//	int num2;
+//	int quotient;
+//	int remainder;
+//
+//	scanf("%d %d", &num1, &num2);
+//
+//	quotient = getQuotientAndRemainder(num1, num2, &remainder);
+//
+//	printf("%d %d", quotient, remainder);
+//	return 0;
+//}
---
+++ unit67/jucge_fibonacci_number.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int fib(int num)
+//{
+//	if ((num == 1) || (num == 2)) {
+//		return 1;
+//	}
+//
+//	return fib(num - 1) + fib(num - 2);
+//}
+//
+//int main()
+//{
+//	int num1;
+//	scanf("%d", &num1);
+//
+//	printf("%d\n", fib(num1));
+//
+//	return 0;
+//}
